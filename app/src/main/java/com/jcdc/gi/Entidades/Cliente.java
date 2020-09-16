c16d4395a2561313da624d02ab6c8d6e7e659934
jcdc2802 <69880704+jcdc2802@users.noreply.github.com>
2020-09-16 02:42:20 +0000

    se prueba el pull

--- app/src/main/java/com/jcdc/gi/Entidades/Cliente.java
+++ app/src/main/java/com/jcdc/gi/Entidades/Cliente.java
@@ -1,5 +1,18 @@
 package com.jcdc.gi.Entidades;
+import android.support.v7.app.*;
 
-public class Cliente
+public class Cliente extends AppCompatActivity
 {
+    public String id;
+	  public String nombre;
+	  public String nit;
+	  public String celular;
+	  public String mail;
+	  public String estado;
+	
+    public Cliente(){}
+    
+    
+    
+    
 }
